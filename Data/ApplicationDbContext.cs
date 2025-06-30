fbf0f593540e65adb67477c7ba0522459fe017ce
Chien Khuat Anh <kachien.dhti17a4hn@sv.uneti.edu.vn>
2025-06-30 22:48:02 +0700

    Initial commit for Book Management App

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,7 +9,18 @@ namespace MyWebApp3.Data
         {
 
         }
-        
-        public DbSet<Book> Books {get; set;}
+
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Đắc Nhân Tâm", Author = "Dale Carnegie", YearPublished = 1936 },
+                new Book { Id = 2, Title = "Nhà Giả Kim", Author = "Paulo Coelho", YearPublished = 1988 },
+                new Book { Id = 3, Title = "Cha Giàu Cha Nghèo", Author = "Robert Kiyosaki", YearPublished = 1997 }
+            );
+        }
     }
+    
 }
